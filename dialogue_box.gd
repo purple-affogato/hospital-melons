362a81fe372a134cca2c5d1fe7dfc242a8547bd4
purple-affogato <ivy.yiting.zhuang@gmail.com>
2023-07-21 17:46:13 -0400

    the laundry machine now changes colours wheeee

--- dialogue_box.gd
+++ dialogue_box.gd
@@ -3,9 +3,9 @@ extends Node2D
 @onready var label = $RichTextLabel
 
 func _process(delta):
-	if Input.is_key_pressed(KEY_ENTER):
+	if Input.is_key_pressed(KEY_ENTER): # this is how you check for the enter key
 		set_label("what")
 
 func set_label(string):
-	label.text = string
+	label.text = string # and this is how you change the text label
 
